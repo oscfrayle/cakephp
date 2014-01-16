6c1bd6531675fe279e8763a4105f4587a2f740d4
oscfrayle <oscfrayle@gmail.com>
2014-01-16 12:09:34 -0500

    cuatro

--- app/Controller/PagesController.php
+++ app/Controller/PagesController.php
@@ -2,14 +2,6 @@
 
 App::uses('AppController', 'Controller');
 
-/**
- * Static content controller
- *
- * Override this controller by placing a copy in controllers directory of an application
- *
- * @package       app.Controller
- * @link http://book.cakephp.org/2.0/en/controllers/pages-controller.html
- */
 class PagesController extends AppController {
 
 /**
